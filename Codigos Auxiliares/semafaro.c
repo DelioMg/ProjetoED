11d87855edecd8db8914881f899fab86c0dc1839
Délio <delio_mg@hotmail.com>
2022-04-02 01:29:49 -0300

    Update semafaro.c

--- Codigos Auxiliares/semafaro.c
+++ Codigos Auxiliares/semafaro.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>// necessário p/ as funções rand() e srand()
 #include<stdio.h>
 #include<time.h>//necessário p/ função time()
+#define MAX 10
 
 int ncarros1=0, ncarros2=0; //Numero de Carros
 int pcarro1=0, pcarro2=0; //Tempo que proximo carro vai entrar na fila 
@@ -10,7 +11,7 @@ int c1=0, c2=0; //conta tempo para proximo carro
 int t1=0, t2=0; //tempo de cada cor, incremento equivale a 5s
 
 int main(void){
-
+int aux;
     
     printf("\n        Programa Semafaro\n");
     srand(time(NULL));
@@ -24,7 +25,7 @@ int main(void){
         
 int contador = 1; 
   
-  while (contador <= 10) 
+  for(contador = 1; contador <= 10; contador++)
   {
       srand(time(NULL));
 printf("\n");
@@ -36,105 +37,68 @@ printf("semaforo 1 - Verde\n");
         ncarros1=0;
     }
     printf("%d carros na fila\n",ncarros1);//Listar carrros que estão na pilha1 
-    if(c1 == 0){
-    pcarro1= rand() % 30; // gerando valores aleatórios na faixa de 0 a 30 para tempo que proximo carro vai entrar na fila
-    c1= pcarro1/5;
-      if(c1 == 0){
-        printf("Novo carro no semafaro 1\n");
-        //insere um carro na fila 
-        ncarros1 ++;
-        c1=0;
-        t1=0;  
-      }
-      t1++;
+    aux = rand() % 3;
+    if((aux + ncarros1)<MAX){
+	ncarros1= ncarros1 + aux;
+    	
+    	printf("%d Novo(s) carro(s) no semafaro 1\n", aux);
+    	printf("Total de %d carros no semafaro 1\n", ncarros1);
+    }else {
+    	ncarros1 = MAX;
+    	printf("fila cheia de carros. %d carros no total.\n",ncarros1);
     }
-    if(c1 == t1){
-        printf("Novo carro no semafaro 1\n");
-        //insere um carro na fila 
-        ncarros1 ++;
-        c1=0;
-        t1=0;
-    }
+    
 
-    
-    
     printf("semaforo 2 - Vermelho\n");
     printf("%d carros na fila\n",ncarros2);//Listar carrros que estão na pilha2
-    if(c2 == 0){
-    pcarro2= rand() % 30; // gerando valores aleatórios na faixa de 0 a 30 para tempo que proximo carro vai entrar na fila
-    c2= pcarro2/5;
-      if(c2 == 0){
-        printf("Novo carro no semafaro 2\n");
-        //isere um carro na fila 2 
-        ncarros2 ++;
-        c2=0;
-        t2=0;  
-      }
-      t2++;
+     aux = rand() % 3;
+     if((aux + ncarros2)<MAX){
+	ncarros2= ncarros2 + aux;
+    	
+    	printf("%d Novo(s) carro(s) no semafaro 2\n", aux);
+    	printf("Total de %d carros no semafaro 2\n\n\n", ncarros2);
+    }else {
+    	ncarros2 = MAX;
+    	printf("fila cheia de carros. %d carros no total.\n\n\n",ncarros2);
     }
-    if(c2 == t2){
-        printf("Novo carro no semafaro 2\n");
-        //isere um carro na fila 2
-        ncarros2 ++;
-        c2=0;
-        t2=0;
-    }
-    
-    //Segundo sinal
-    printf("\n");
-    printf("semaforo 1 - Vermelho\n");
-    printf("%d carros na fila\n",ncarros1);//Listar carrros que estão na pilha1
-    if(c1 == 0){
-    pcarro1= rand() % 30; // gerando valores aleatórios na faixa de 0 a 30 para tempo que proximo carro vai entrar na fila
-    c1= pcarro2/5;
-      if(c1 == 0){
-        printf("Novo carro no semafaro 2\n");
-        //insere um carro na fila 2 
-        ncarros1 ++;
-        c1=0;
-        t1=0;  
-      }
-      t1++;
-    }
-    if(c1 == t1){
-        printf("Novo carro no semafaro 2\n");
-        //insere um carro na fila 2
-        ncarros1 ++;
-        c1=0;
-        t1=0;
-    }
-     
 
     
+
+    printf("semaforo 1 - Vermelho\n");
+    printf("%d carros na fila\n",ncarros1);//Listar carrros que estão na pilha2
+     aux = rand() % 3;
+     if((aux + ncarros1)<MAX){
+	ncarros1= ncarros1 + aux;
+    	
+    	printf("%d Novo(s) carro(s) no semafaro 1\n", aux);
+    	printf("Total de %d carros no semafaro 1\n", ncarros1);
+    }else {
+    	ncarros1 = MAX;
+    	printf("fila cheia de carros. %d carros no total.\n",ncarros1);
+    }
+    
+    
     printf("semaforo 2 - Verde\n");
-    //retira 3 carros da fila
-        if(ncarros2 > 3){
+         //retira 3 carros da fila
+    if(ncarros2 > 3){
         ncarros2= ncarros2 -3;
     }else {
         ncarros2=0;
     }
-    printf("%d carros na fila\n",ncarros2);//Listar carrros que estão na pilha2
+    printf("%d carros na fila\n",ncarros2);//Listar carrros que estão na pilha1 
+    aux = rand() % 3;
+    if((aux + ncarros2)<MAX){
+	ncarros2= ncarros2 + aux;
+    	
+    	printf("%d Novo(s) carro(s) no semafaro 2\n", aux);
+    	printf("Total de %d carros no semafaro 2\n", ncarros2);
+    }else {
+    	ncarros2 = MAX;
+    	printf("fila cheia de carros. %d carros no total.\n\n\n",ncarros2);
+    }
     
-    if(c2 == 0){
-    pcarro2= rand() % 30; // gerando valores aleatórios na faixa de 0 a 30 para tempo que proximo carro vai entrar na fila
-    c2= pcarro2/5;
-      if(c2 == 0){
-        printf("Novo carro no semafaro 1\n");
-        //insere um carro na fila 
-        ncarros2 ++;
-        c2=0;
-        t2=0;  
-      }
-      t2++;
-    }
-    if(c2 == t2){
-        printf("Novo carro no semafaro 1\n");
-        //insere um carro na fila 
-        ncarros2 ++;
-        c2=0;
-        t2=0;
-    }
-    contador++;
+    
+   
   }
 
 }
